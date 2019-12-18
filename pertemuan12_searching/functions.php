631fa77c90e9ddd610eae827a61fc01f5b916e18
MYusron12 <ryusron19@gmail.com>
2019-12-18 16:37:50 +0700

    php fungsi upload

--- pertemuan12_searching/functions.php
+++ pertemuan12_searching/functions.php
@@ -21,13 +21,56 @@ function tambah($data)
     $nama = htmlspecialchars($data["nama"]);
     $email = htmlspecialchars($data["email"]);
     $jurusan = htmlspecialchars($data["jurusan"]);
-    $gambar = htmlspecialchars($data["gambar"]);
+
+    //upload gambar
+    $gambar = upload();
+    if (!$gambar) {
+        return false;
+    }
 
     //lakukan query insert data
     $query = "INSERT INTO mahasiswa VALUES('','$nrp','$nama','$email','$jurusan','$gambar')";
     mysqli_query($conn, $query);
 
     return mysqli_affected_rows($conn);
+}
+
+
+function upload()
+{
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    //cek apakah tidak ada gambar yang di upload
+    if ($error === 4) {
+        echo "<script>
+                    alert('pilih gambar terlebih dahulu');
+                </script>";
+        return false;
+    }
+    //cek apakh yang di upload adalah gambar
+    $ekstensiGambarValid = ['jpg', 'jpeg', 'png'];
+    $ekstensiGambar = explode('.', $namaFile);
+    $ekstensiGambar = strtolower(end($ekstensiGambar));
+    if (!in_array($ekstensiGambar, $ekstensiGambarValid)) {
+        echo "<script>
+                alert('yang anda upload bukan gambar');
+            </script>";
+        return false;
+    }
+    //cek jika ukurannya terlalu besar
+    if ($ukuranFile > 1000000) {
+        echo "<script>
+                alert('ukuran gambar terlalu besar');
+            </script>";
+        return false;
+    }
+    //lolos pengecekan, gambar siap diupload
+    move_uploaded_file($tmpName, 'img/' . $namaFile);
+
+    return $namaFile;
 }
 
 function hapus($id)
